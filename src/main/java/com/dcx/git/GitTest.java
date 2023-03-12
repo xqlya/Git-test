46ba2127db7a73465a724d8a8610f9e4e49269e4
dcx <dcxtop@qq.com>
2023-03-12 21:43:44 +0800

    第五次提交

--- src/main/java/com/dcx/git/GitTest.java
+++ src/main/java/com/dcx/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git 你好！");
         System.out.println("Git 你再好！");
-        System.out.println("正常合并分支");
+        System.out.println("hot-fix 测试");
     }
 }
